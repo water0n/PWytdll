a58d7130590211576b5716749ab95456b707c358
Daniel Zermeño <gerardo.zermeno@softrestaurant.com>
2026-04-20 12:23:53 -0600

    Limpieza de repositorio

--- check_functions.ps1
+++
@@ -1,6 +0,0 @@
-$content = Get-Content -Path "c:\Users\water\Documents\githubProyects\YTDLL\Functions.ps1"
-for ($i = 0; $i -lt $content.Count; $i++) {
-    if ($content[$i] -match "(?i)Drawing\.Color|Windows\.Forms|lblEstadoConsulta") {
-        Write-Output "Line $($i+1): $($content[$i].Trim())"
-    }
-}
--- encode.ps1
+++
@@ -1,2 +0,0 @@
-$content = Get-Content -LiteralPath '.\GUI.ps1' -Raw -Encoding UTF8
-[System.IO.File]::WriteAllText("c:\Users\water\Documents\githubProyects\YTDLL\GUI.ps1", $content, [System.Text.Encoding]::UTF8)
